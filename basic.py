122b7f83381b761738f0a72461c9d810daf08b46
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-14 14:41:49 +0530

    Basics

---
+++ basic.py
@@ -0,0 +1,17 @@
+print("Hello World !")
+print("I am learning Python.")
+print("It is awesome!")
+
+x = 4 
+y = "Sally"
+z = "3.5"
+
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
