18ef9a94a5819107fa684451040499ddba26058a
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-17 13:00:21 +0530

    Python Operators

--- basic.py
+++ basic.py
@@ -1,17 +1,78 @@
-print("Hello World !")
-print("I am learning Python.")
-print("It is awesome!")
+# print("Hello World !")
+# print("I am learning Python.")
+# print("It is awesome!")
 
-x = 4 
-y = "Sally"
-z = "3.5"
+# x = 4 
+# y = "Sally"
+# z = "3.5"
 
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
 
-fruits = ["apple", "banana", "cherry"]
-x,y,z = fruits
-print(x)
-print(y)
-print(z)
+# fruits = ["apple", "banana", "cherry"]
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x1 = "Hello World"
+# x2 = 20
+# x7 = range(5)
+
+#Integers
+x= int(1)
+y = int(2.8)
+z= int("3")
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Floats:
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x=15
+y=4
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+# x=5
+# x=x+3
+# x=x-3
+# x=x*3
+# x=x/3
+# x=x%3
+# x=x//3
+# x=x**3
+# x=x&3
+# x=x|3
+# x=x^3
+# x=x>>3
+# x=x<<3
+
+
+x=5
+y=3
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
